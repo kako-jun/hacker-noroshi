87416783b00bf6b127609a45b299203ff9fbc47d
kako-jun <3541096+kako-jun@users.noreply.github.com>
2026-04-15 10:30:59 +0900

    feat: kako-jun/hacker-noroshi#38 /listsページを追加（ブラウズリンク集）

---
+++ src/routes/lists/+page.svelte
@@ -0,0 +1,47 @@
+<div class="lists-page">
+	<table>
+		<tr>
+			<td><a href="/front">front</a></td>
+			<td>past stories</td>
+		</tr>
+		<tr>
+			<td><a href="/newcomments">newcomments</a></td>
+			<td>new comments</td>
+		</tr>
+		<tr>
+			<td><a href="/best">best</a></td>
+			<td>best stories</td>
+		</tr>
+		<tr>
+			<td><a href="/show">show</a></td>
+			<td>Show HN</td>
+		</tr>
+		<tr>
+			<td><a href="/ask">ask</a></td>
+			<td>Ask HN</td>
+		</tr>
+	</table>
+</div>
+
+<style>
+	.lists-page {
+		padding: 10px 0 10px 40px;
+		font-size: 10pt;
+	}
+
+	.lists-page table {
+		border-spacing: 0;
+	}
+
+	.lists-page td {
+		padding: 2px 5px;
+	}
+
+	.lists-page td:first-child a {
+		color: #000000;
+	}
+
+	.lists-page td:last-child {
+		color: #828282;
+	}
+</style>
